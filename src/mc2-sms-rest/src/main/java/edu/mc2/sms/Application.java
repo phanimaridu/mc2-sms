427648cd97e3d52eb61a4b58149e70a203f00b36
phanimaridu <phani.vjec@gmail.com>
2015-04-13 19:03:42 -0400

    Removed bad comments

--- src/mc2-sms-rest/src/main/java/edu/mc2/sms/Application.java
+++ src/mc2-sms-rest/src/main/java/edu/mc2/sms/Application.java
@@ -3,12 +3,6 @@ package edu.mc2.sms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
-http://localhost:8081/admin
-http://localhost:8081/staff
-http://localhost:8081/student
-http://localhost:8081/anonymous
- */
 @SpringBootApplication
 public class Application {
 
