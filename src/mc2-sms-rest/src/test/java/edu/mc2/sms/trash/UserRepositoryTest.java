803620aa1e3c21d1be1c26ac395a6ae354664612
phanimaridu <phani.vjec@gmail.com>
2015-06-03 02:07:28 -0400

    test cases removed

--- src/mc2-sms-rest/src/test/java/edu/mc2/sms/trash/UserRepositoryTest.java
+++ src/mc2-sms-rest/src/test/java/edu/mc2/sms/trash/UserRepositoryTest.java
@@ -1,4 +1,4 @@
-package edu.mc2.sms.dao;
+package edu.mc2.sms.trash;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -9,14 +9,17 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import edu.mc2.sms.Application;
 import edu.mc2.sms.jpa.repository.UserRepository;
 
+@WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class UserRepositoryTest {
 
+	
 
 	@Autowired
 	private UserRepository userDAO;
@@ -46,8 +49,7 @@ public class UserRepositoryTest {
 	
 	@Test
 	public void test() {
-		//System.out.println(">>>>>>>>>>>>>>" + userDAO.count());
-		//System.out.println(">>>>>>>>>>>>>>" +service.getAttendanceReport(1, null, null));
+
 		
 	}
 
